b8f71daedb8a0ba191e7f9f2f8cba4a05b09bd0d
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 18:12:57 +0200

    ex3 partie3 php

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
-$year = array('janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre');
 $months = array ('janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre');
 ?>
-    <!DOCTYPE html>
-  <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>ex3 partie3 php</title>
-  </head>
-  <body>
-    <?php
-    foreach ($year as $months){
-      echo'- '.$months.;
-    }
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex3 partie3 php</title>
+</head>
+<body>
+  <?php
+  foreach ($months as $month){ //boucle foreach - months nom tableau et month variable (nommer comme on veut)
     ?>
-  </body>
-  </html>
+    <p>- <?php echo $month; ?></p>
+  <?php } ?>
+</body>
+</html>
